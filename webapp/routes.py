ae9e0ed90d1ae4e427caa554ecbf9c74d7365c84
Aryan Ramani <aryanramani67@gmail.com>
2024-01-04 18:43:40 +0530

    Developed API

--- webapp/routes.py
+++ webapp/routes.py
@@ -13,10 +13,23 @@ def home_page():
 
 @app.route('/predict', methods=['GET', 'POST'])
 def predict():
-    predictions = ''
+    preds = ''
     text = ''
     if request.method == 'POST':
         text = request.form.get('user_input')
         predictions = predict_model.get_predictions(text)
-    return render_template('index.html', prediction_text=predictions, input_text=text)
+        for model, prediction in predictions.items():
+            preds += f'{model} : {str(prediction[0])}'
+            c = ',  ' if model == 'LogisticRegression' else ' '
+            preds += c
+    return render_template('index.html', prediction_text=preds, input_text=text)
+
+
+@app.route('/predict-api', methods=['POST'])
+def predict_api():
+    data = request.json['data']
+    text = data['text']
+    predictions = predict_model.get_predictions(text)
+    predictions = { model:str(prediction[0]) for model, prediction in predictions.items()}
+    return jsonify(predictions)
 
